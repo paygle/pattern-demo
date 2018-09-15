a41f89fbc10a06b2684aa065064b7b52f2dfd791
paygle <dodar@qq.com>
2018-09-16 00:13:08 +0800

    update config

--- www/config/config.js
+++ www/config/config.js
@@ -1,7 +1,9 @@
 
 // ref: https://umijs.org/config/
 export default {
-  publicPath: '/static/',
+  publicPath: '/',
+  // exportStatic: { htmlSuffix: true }, // 静态化选项
+  // dynamicRoot: true, // 静态化到任何目录
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react', {
